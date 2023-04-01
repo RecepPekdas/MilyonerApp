c20367528012db6c394f4712ec6fdd2c48ad764d
RecepPekdas <receppekdas3460@gmail.com>
2023-04-01 11:17:49 +0300

    feat: def variables

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MilyonerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Kim Milyoner Olmak İster - V1
+            Console.WriteLine("Kim Milyoner Olmak İster\n");
+            Console.Write("İsim Giriniz: ");
+            string isim = Console.ReadLine();
+            Console.Write("Soyisim Giriniz: ");
+            string soyisim = Console.ReadLine();
+        }
+    }
+}
