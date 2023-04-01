89b799b9e37ce7872d187f3ce32dd73bfb842686
RecepPekdas <receppekdas3460@gmail.com>
2023-04-01 11:30:40 +0300

    feat: add revert control

--- Program.cs
+++ Program.cs
@@ -136,6 +136,21 @@
 
                 goto yanitNoktasi;
             }
+            else if (secim == 'R')
+            {
+                Console.WriteLine(kasa + " TL Kazanacaksınız.");
+                Console.Write("Çekilmek İstediğinize Emin Misiniz? : ");
+                char cekilCevap = char.Parse(Console.ReadLine());
+
+                if (cekilCevap == 'E')
+                {
+                    Console.WriteLine("Tebrikler, " + kasa + " TL Kazandınız.");
+                    Console.WriteLine("Oyun Bitti");
+                    //return;
+                    Environment.Exit(0);
+                }
+                goto soruNoktasi;
+            }
 
 
         }
